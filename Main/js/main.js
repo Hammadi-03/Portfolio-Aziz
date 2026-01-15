384e62150b947446fbbaee2f3ab072679ff7b5b7
Hammadi-03 <[200~abdulazizhammadi809@gmail.com>
2026-01-15 15:00:40 +0700

    innit

--- Main/js/main.js
+++ Main/js/main.js
@@ -1,5 +1,19 @@
+import { gsap } from "gsap";
+const { gsap } = require("gsap/dist/gsap");
+
+import { ScrollTrigger } from "ScrollTrigger.js";
+
 gsap.registerPlugin(ScrollTrigger);
 
+import { gsap } from "gsap";
+
+import { CustomEase } from "gsap/CustomEase";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,CustomEase);
 const textElements = gsap.utils.toArray('.text');
 
 textElements.forEach(text => {
@@ -89,9 +103,9 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
   wrapper: "#smooth-wrapper",
   content: "#smooth-content",
-  smooth: 2.5, // much softer and slower catch-up for smoothness
+  smooth: 4.5, // much softer and slower catch-up for smoothness
   effects: true, // looks for data-speed and data-lag attributes on elements
-  smoothTouch: 0.05 // even softer smoothing on touch devices
+  smoothTouch: 0.07 // even softer smoothing on touch devices
 });
 
 import React, { useState } from "https://esm.sh/react";
@@ -376,3 +390,30 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+const images = gsap.utils.toArray('img');
+const loader = document.querySelector('.loader--text');
+const updateProgress = (instance) =>
+loader.textContent = `${Math.round(instance.progressedCount * 100 / images.length)}%`;
+
+const showDemo = () => {
+  document.body.style.overflow = 'auto';
+  document.scrollingElement.scrollTo(0, 0);
+  gsap.to(document.querySelector('.loader'), { autoAlpha: 0 });
+
+  gsap.utils.toArray('section').forEach((section, index) => {
+    const w = section.querySelector('.wrapper');
+    const [x, xEnd] = index % 2 ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+    gsap.fromTo(w, { x }, {
+      x: xEnd,
+      scrollTrigger: {
+        trigger: section,
+        scrub: 0.5 } });
+
+
+  });
+};
+
+imagesLoaded(images).on('progress', updateProgress).on('always', showDemo);
